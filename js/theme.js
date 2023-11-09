aae3a15f96a4e6297e0699c06a35cb13f13230da
GanzhaSS <ganzhass@nornik.ru>
2023-11-09 18:37:13 +0300

    Renamed classes, removed button...

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,5 @@
 // Выбираем кнопку
-const btn = document.querySelector(".btn-toggle");
+const btn = document.querySelector(".____btn_mode");
 // Выбираем таблицу стилей
 const theme = document.querySelector("#theme-link");
 // Отслеживаем щелчок по кнопке
@@ -8,9 +8,11 @@ btn.addEventListener("click", function () {
     if (theme.getAttribute("href") == "style/default_light.css") {
         // …то переключаемся на "dark-theme.css"
         theme.href = "style/default_dark.css";
+        btn.innerHTML = "Light mode";
         // В противном случае… 
     } else {
         // …переключаемся на "light-theme.css"
         theme.href = "style/default_light.css";
+        btn.innerHTML = "Dark mode";
     }
 });
