d8954bfe59c658d1f3e685c72a8d060e3c6d33cb
ganzhass <ganzhass@nornik.ru>
2023-11-09 23:20:38 +0300

    Changed mode

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,7 @@
 // Выбираем кнопку
-const btn = document.querySelector(".____btn_mode");
-// Выбираем таблицу стилей
-const theme = document.querySelector("#theme-link");
+const btn = document.querySelector('.btn_mode');
 // Отслеживаем щелчок по кнопке
-btn.addEventListener("click", function () {
-    // Если текущий адрес содержит "light-theme.css"
-    if (theme.getAttribute("href") == "style/default_light.css") {
-        // …то переключаемся на "dark-theme.css"
-        theme.href = "style/default_dark.css";
-        btn.innerHTML = "Light mode";
-        // В противном случае… 
-    } else {
-        // …переключаемся на "light-theme.css"
-        theme.href = "style/default_light.css";
-        btn.innerHTML = "Dark mode";
-    }
-});
+btn.addEventListener('click', function () {
+    // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
+    document.body.classList.toggle('dark-theme');
+})
