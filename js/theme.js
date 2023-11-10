56732bf4d8c61d3b071841d7ff5b13c09665d459
GanzhaSS <ganzhass@nornik.ru>
2023-11-10 11:33:19 +0300

    Deleted files, dark/light, colors

--- js/theme.js
+++ js/theme.js
@@ -4,4 +4,7 @@ const btn = document.querySelector('.btn_mode');
 btn.addEventListener('click', function () {
     // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
     document.body.classList.toggle('dark-theme');
+    if (document.body.classList == 'dark-theme') {
+
+    }
 })
