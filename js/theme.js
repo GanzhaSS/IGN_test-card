fb84b386a2180c812ec08b2f81d4d77137e52ab9
GanzhaSS <ganzhass@nornik.ru>
2023-11-10 11:37:10 +0300

    Changed button label Dark/Light mode

--- js/theme.js
+++ js/theme.js
@@ -5,6 +5,8 @@ btn.addEventListener('click', function () {
     // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
     document.body.classList.toggle('dark-theme');
     if (document.body.classList == 'dark-theme') {
-
+        btn.innerHTML = "Light mode";
+    } else {
+        btn.innerHTML = "Dark mode";
     }
 })
